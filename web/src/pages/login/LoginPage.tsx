05db43b74dd2978b3bd2736e8a7963646c708ae4
Wes Lord <weslord@fisgardlabs.com>
2024-02-09 12:11:59 -0800

    Split Input component from login to own module

--- web/src/pages/login/LoginPage.tsx
+++ web/src/pages/login/LoginPage.tsx
@@ -1,23 +1,12 @@
-const InputBox = ({ label, placeholder, type }: { label: string; placeholder?: string; type?: 'password' }) => {
-  return (
-    <div className="mt-2">
-      <label className="block text-title font-semibold">{label}</label>
-      <input
-        placeholder={placeholder}
-        type={type}
-        className="w-full shadow-input rounded-md p-3 text-sm text-input-text bg-input-background"
-      />
-    </div>
-  )
-}
+import { Input } from '../../components/Input'
 
 export const LoginPage: React.FC = () => {
   return (
     <div className="w-96 mx-auto">
       <h2 className="text-4xl font-bold text-title mt-32 mb-16">Log in to your account</h2>
 
-      <InputBox label="Email" placeholder="jamie.ohara@example.ca" />
-      <InputBox label="Password" placeholder="********" type="password" />
+      <Input label="Email" placeholder="jamie.ohara@example.ca" />
+      <Input label="Password" placeholder="********" type="password" />
 
       <form action="/dashboard">
         <button
