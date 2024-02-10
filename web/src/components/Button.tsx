4a746b00a2576ba295345c942ee126253775d7da
Wes Lord <weslord@fisgardlabs.com>
2024-02-10 14:56:48 -0800

    Add PrimaryButton component
    Signed-off-by: Wes Lord <weslord@fisgardlabs.com>

---
+++ web/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { ButtonHTMLAttributes } from 'react'
+
+export const PrimaryButton = ({
+  type,
+  children,
+}: {
+  type?: ButtonHTMLAttributes<undefined>['type']
+  children: React.ReactNode
+}) => (
+  <button
+    type={type || 'button'}
+    className="flex justify-center items-center w-full h-12 mt-8 bg-primary-background rounded-md font-semibold text-lg text-primary-text"
+  >
+    {children}
+  </button>
+)
