dcf48b2ee46ed68668420a54558398a1c3ef3403
Wes Lord <weslord@fisgardlabs.com>
2024-02-11 13:44:09 -0800

    Add link from video list to review page
    Signed-off-by: Wes Lord <weslord@fisgardlabs.com>

--- web/src/pages/dashboard/components/VideoCard.tsx
+++ web/src/pages/dashboard/components/VideoCard.tsx
@@ -15,9 +15,12 @@ export const VideoCard: React.FC<Props> = ({ video }) => {
     <div className="flex flex-col overflow-hidden justify-between border shrink-0 grow-0 rounded-xl w-72 h-96">
       <div className="m-3">Uploaded {video.upload_date.toLocaleDateString('en-CA', date_format)}</div>
       <div className="flex justify-center items-center px-4 py-6 bg-off-white">
-        <div className="opacity-100 flex justify-center items-center w-full h-10 rounded-lg bg-slate font-bold">
+        <a
+          href={`/dashboard/review/${video.id}`}
+          className="opacity-100 flex justify-center items-center w-full h-10 rounded-lg bg-slate font-bold"
+        >
           Review
-        </div>
+        </a>
       </div>
     </div>
   )
